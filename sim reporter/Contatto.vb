94761dc02b0f6301938c69d05ab9965719071cac
Alex Cortinovis <walker1993@live.it>
2016-01-18 14:30:06 +0100

    progresso

--- sim reporter/Contatto.vb
+++ sim reporter/Contatto.vb
@@ -9,7 +9,7 @@
     Public Const ParameterSize As Integer = 7
 
 
-    Sub New(Optional n = "", Optional t = "", Optional a = "", Optional at = "", Optional gr = "", Optional emln = 0, Optional snn = 0)
+    Sub New(Optional n = "", Optional t = "", Optional a = "", Optional at = "255", Optional gr = "0000000000FFFFFFFFFF", Optional emln = 255, Optional snn = 255)
         Me.n = n
         Me.t = t
         Me.a = a
@@ -18,4 +18,29 @@
         Me.emln = emln
         Me.snn = snn
     End Sub
+
+    Public Shared Operator =(contact1 As Contatto, contact2 As Contatto) As Boolean
+
+        If contact1.n <> contact2.n Then Return False
+        If contact1.t <> contact2.t Then Return False
+        If contact1.a <> contact2.a Then Return False
+        If contact1.at <> contact2.at Then Return False
+        If contact1.gr <> contact2.gr Then Return False
+        If contact1.emln <> contact2.emln Then Return False
+        If contact1.snn <> contact2.snn Then Return False
+        Return True
+
+    End Operator
+
+    Public Shared Operator <>(contact1 As Contatto, contact2 As Contatto) As Boolean
+        If contact1.n <> contact2.n Then Return True
+        If contact1.t <> contact2.t Then Return True
+        If contact1.a <> contact2.a Then Return True
+        If contact1.at <> contact2.at Then Return True
+        If contact1.gr <> contact2.gr Then Return True
+        If contact1.emln <> contact2.emln Then Return True
+        If contact1.snn <> contact2.snn Then Return True
+        Return False
+
+    End Operator
 End Class
--- sim reporter/Form3.vb
+++ sim reporter/Form3.vb
@@ -1,10 +1,14 @@
 ﻿Public Class Form3
     Dim doc As HtmlDocument
-
+    Dim rubrica_n() As String = {"rubrica_nome", "rubrica_cognome", "rubrica_numero", "rubrica_email", "rubrica_num_alt", "rubrica_gruppo", "rubrica_categoria"}
+    Dim messaggi_n() As String = {"messaggi_stato", "messaggi_nome", "messaggi_numero", "messaggi_data"}
+    Dim registro_n() As String = {"registro_nome", "registro_numero"}
+    Dim op_n() As String = {"op_MCC", "op_MNC"}
     Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles web.DocumentCompleted
         doc = web.Document
         fill_riepilogo()
         fill_SimInfo()
+        fill_rubrica()
     End Sub
 
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -21,8 +25,59 @@
 
     Sub fill_SimInfo()
         doc.GetElementById("ICCID").InnerText = info_CSV(0).Item2
-        doc.GetElementById("IMSI").InnerText = info_CSV(1).Item2
-        doc.GetElementById("fornitore").InnerText = info_CSV(2).Item2
+        doc.GetElementById("IMSI").InnerText = info_CSV(2).Item2
+        doc.GetElementById("fornitore").InnerText = info_CSV(1).Item2
     End Sub
 
+    Sub fill_rubrica()
+        Dim rubrica As HtmlElement = doc.GetElementById("rubrica")
+        For i = 0 To contacts.Count - 1 'numero di righe (in questo caso contatti)
+            If contacts(i) = (New Contatto()) Then Continue For
+            Dim riga_element As HtmlElement = doc.CreateElement("tr")
+            For j = 0 To Contatto.ParameterSize - 1 'numero di paramentri
+                Dim td As HtmlElement = doc.CreateElement("td")
+                td.Name = rubrica_n(j)
+                Select Case j
+                    Case 0
+                        td.InnerText = contacts(i).n
+                    Case 1
+                        Try
+                            td.InnerText = SNM(contacts(i).snn)
+                        Catch
+                            td.InnerText = ""
+                        End Try
+                    Case 2
+                        td.InnerText = contacts(i).t
+                    Case 3
+                        Try
+                            td.InnerText = EmL(contacts(i).emln)
+                        Catch
+                            td.InnerText = ""
+                        End Try
+                    Case 4
+                        td.InnerText = contacts(i).a
+                    Case 5
+                        td.InnerText = If(contacts(i).gr = "0000000000FFFFFFFFFF", "", groups(contacts(i).gr))
+                    Case 6
+                        Try
+                            td.InnerText = categories(contacts(i).at)
+                        Catch
+                            td.InnerText = ""
+                        End Try
+                End Select
+
+                riga_element.AppendChild(td)
+            Next
+            rubrica.FirstChild.AppendChild(riga_element)
+        Next
+
+
+
+
+        'Dim y = 0
+        'For Each nome As HtmlElement In doc.GetElementsByTagName("td").GetElementsByName("rubrica_nome")
+        '    nome.InnerText = contacts(y).n
+        '    y += 1
+        'Next
+    End Sub
 End Class
